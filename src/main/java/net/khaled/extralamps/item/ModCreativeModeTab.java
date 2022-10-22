73b28ad1dd4284d825cdeb84cabc4a8f5bed1082
Khaled-Murrar <khalouchdev@gmail.com>
2022-10-22 20:13:35 +0300

    Item group name

--- src/main/java/net/khaled/extralamps/item/ModCreativeModeTab.java
+++ src/main/java/net/khaled/extralamps/item/ModCreativeModeTab.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTab {
-    public static final CreativeModeTab LAMPS = new CreativeModeTab("Extra Lamps") {
+    public static final CreativeModeTab LAMPS = new CreativeModeTab("ExtraLamps") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModBlocks.BLUE_LAMP.get());    //return new ItemStack(ModItems.BLUE_LAMP.get())
