2b770e1d94557f3e7353166f161bdb233676328b
Khaled-Murrar <khalouchdev@gmail.com>
2022-10-22 21:29:04 +0300

    Register All Lamps

--- src/main/java/net/khaled/extralamps/block/ModBlocks.java
+++ src/main/java/net/khaled/extralamps/block/ModBlocks.java
@@ -32,6 +32,28 @@ public class ModBlocks {
             () -> new MagentaLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(MagentaLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
     public static final RegistryObject<Block> YELLOW_LAMP = registerBlock("yellow_lamp",
             () -> new YellowLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(YellowLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> ORANGE_LAMP = registerBlock("orange_lamp",
+            () -> new OrangeLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(OrangeLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> LIME_LAMP = registerBlock("lime_lamp",
+            () -> new LimeLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(LimeLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> CYAN_LAMP = registerBlock("cyan_lamp",
+            () -> new CyanLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(CyanLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> PINK_LAMP = registerBlock("pink_lamp",
+            () -> new PinkLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(PinkLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> LIGHT_BLUE_LAMP = registerBlock("light_blue_lamp",
+            () -> new LightBlueLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(LightBlueLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> BROWN_LAMP = registerBlock("brown_lamp",
+            () -> new BrownLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(BrownLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> GRAY_LAMP = registerBlock("gray_lamp",
+            () -> new GrayLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(GrayLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> LIGHT_GRAY_LAMP = registerBlock("light_gray_lamp",
+            () -> new LightGrayLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(LightGrayLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> WHITE_LAMP = registerBlock("white_lamp",
+            () -> new WhiteLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(WhiteLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> BLACK_LAMP = registerBlock("black_lamp",
+            () -> new BlackLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(BlackLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
+    public static final RegistryObject<Block> Purple_LAMP = registerBlock("purple_lamp",
+            () -> new PurpleLampBlock(BlockBehaviour.Properties.of(Material.GLASS).strength(0.3f).lightLevel(state -> state.getValue(PurpleLampBlock.LIT) ? 15 : 0).sound(SoundType.GLASS)));
 
 
 
